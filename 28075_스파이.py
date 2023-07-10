ec2f700fa4ab9bf1bd477088d7c60ca1dfc46083
Jaeho Jeong <111038793+Jaeho-Jeong@users.noreply.github.com>
2023-07-10 14:25:02 +0900

    Create 28075_스파이.py

---
+++ 28075_스파이.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+progress = [[0] * 3 for _ in range(2)]
+ret = 0
+
+def dfs(day, total, prevJ):
+    global ret
+    if day == N:
+        if total >= M:
+            ret += 1
+        return
+    for i in range(2):
+        for j in range(3):
+            nextTotal = total
+            if j == prevJ:
+                nextTotal += progress[i][j] // 2
+            else:
+                nextTotal += progress[i][j]
+            dfs(day + 1, nextTotal, j)
+
+for i in range(2):
+    progress[i] = list(map(int, input().split()))
+
+dfs(0, 0, -1)
+print(ret)
